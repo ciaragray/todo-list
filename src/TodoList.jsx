b8efa849581e808cadac2cf43d9a3a246c3d2d32
Ciara G <166245306+ciaragray@users.noreply.github.com>
2025-12-12 01:33:59 -0600

    Week 3: add TodoListItem component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,12 +1,11 @@
-import TodoListItem from TodoListItem.jsx; 
+import TodoListItem from './TodoListItem.jsx';
 
 function TodoList({ todos }) {
   return (
     <div>
-      <h1>My Todos</h1>
       <ul>
         {todos.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo}/>
+          <TodoListItem key={todo.id} todo={todo} />
         ))}
       </ul>
     </div>
