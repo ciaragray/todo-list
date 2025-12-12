3ce04c2b3843bd35f799cb2c128001151a22d3c8
Ciara G <166245306+ciaragray@users.noreply.github.com>
2025-12-12 00:58:33 -0600

    Start week 03 compenents:added TodoListItem

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,10 +1,12 @@
+import TodoListItem from TodoListItem.jsx; 
+
 function TodoList({ todos }) {
   return (
     <div>
       <h1>My Todos</h1>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
+          <TodoListItem key={todo.id} todo={todo}/>
         ))}
       </ul>
     </div>
